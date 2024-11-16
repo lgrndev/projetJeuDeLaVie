5bd1ee4f0009b459b543b7f2a1d525ac1a518415
lucas.lgrnd <lucaslangrand@icloud.com>
2024-11-16 16:51:57 +0100

    Ajout de la librairie SFML

--- main.cpp
+++ main.cpp
@@ -1,8 +1,92 @@
+#include <SFML/Graphics.hpp>
 #include "Grille.hpp"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    // Étape 1 : Initialisation
+    int nCols, nRows;
+    cout << "Entrez le nombre de colonnes : ";
+    cin >> nCols;
+    cout << "Entrez le nombre de lignes : ";
+    cin >> nRows;
 
+    Grille grille(nCols, nRows);
+
+    char choix;
+    cout << "Voulez-vous une génération aléatoire (a) ou manuelle (m) ? ";
+    cin >> choix;
+
+    if (choix == 'a') {
+        double proba;
+        cout << "Entrez la probabilité pour une cellule vivante (entre 0 et 1) : ";
+        cin >> proba;
+        grille.repartitionAleatoire(proba);
+    } else if (choix == 'm') {
+        int x;
+        int y;
+        cout << "Entrez les coordonnées des cellules vivantes (-1 -1 pour arrêter) : " << endl;
+        while (true) {
+            cout << "x y : ";
+            cin >> x >> y;
+            if (x == -1 && y == -1) break;
+            if (x >= 0 && x < nRows && y >= 0 && y < nCols) {
+                grille.ajouterCelluleVivante(x, y);
+            } else {
+                cout << "Coordonnées invalides !" << endl;
+            }
+        }
+    }
+
+    cout << "Grille initiale :" << endl;
+    grille.displayGrille();
+
+    // Création de la fenêtre SFML
+    int cellSize = 20;
+    sf::RenderWindow window(sf::VideoMode(nCols * cellSize, nRows * cellSize), "Jeu de la Vie");
+
+    bool continuer = true;
+    while (window.isOpen() && continuer) {
+        // Étape 2 : Boucle principale
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        grille.nextGeneration();
+
+        // Affichage graphique
+        window.clear(sf::Color::Black);
+        for (int y = 0; y < nRows; y++) {
+            for (int x = 0; x < nCols; x++) {
+                sf::RectangleShape cell(sf::Vector2f(cellSize - 1, cellSize - 1));
+                cell.setPosition(x * cellSize, y * cellSize);
+                if (grille.getCellule(y, x) == 1) {
+                    cell.setFillColor(sf::Color::Green);
+                } else {
+                    cell.setFillColor(sf::Color::Black);
+                }
+                window.draw(cell);
+            }
+        }
+        window.display();
+
+        // Affichage console (A enlever)
+        cout << "Génération : " << grille.getNIteration() << endl;
+        grille.displayGrille();
+
+        // Interaction utilisateur
+        cout << "Continuer ? (o/n) : ";
+        cin >> choix;
+        if (choix == 'n') {
+            continuer = false;
+        }
+
+        // Pause pour observer
+        sf::sleep(sf::milliseconds(200));
+    }
     return 0;
 }
